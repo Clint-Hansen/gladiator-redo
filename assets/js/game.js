cfa798bba7d01ce00bbab190bf90771891c5eba3
Clint Hansen <clint.hansen2009@gmail.com>
2021-02-08 15:55:29 -0700

    added randomized fight order feature

--- assets/js/game.js
+++ assets/js/game.js
@@ -83,7 +83,15 @@ var fightOrSkip = function() {
 
 // fight function (now with parameter for enemy's object holding name, health, and attack values)
 var fight = function(enemy) {
+  // keep track of who goes first
+  var isPlayerTurn = true;
+  // randomly change turn order
+  if (Math.random() > 0.5) {
+    isPlayerTurn = false;
+  }
   while (playerInfo.health > 0 && enemy.health > 0) {
+    if (isPlayerTurn) {
+    
     // ask player if they'd like to fight or skip using fightOrSkip function
     if (fightOrSkip()) {
       // if true, leave fight by breaking loop
@@ -110,20 +118,14 @@ var fight = function(enemy) {
       // award player money for winning
       playerInfo.money = playerInfo.money + 20;
 
-      // ask if player wants to use the store before next round
-      var storeConfirm = window.confirm('The fight is over, visit the store before the next round?');
-
-      // if yes, take them to the store() function
-      if (storeConfirm) {
-        shop();
-      }
-
       // leave while() loop since enemy is dead
       break;
-    } else {
-      window.alert(enemy.name + ' still has ' + enemy.health + ' health left.');
-    }
-
+    } 
+  else {
+    window.alert(enemy.name + ' still has ' + enemy.health + ' health left.');
+  }
+  // player gets attacked first
+  } else {
     var damage = randomNumber(enemy.attack - 3, enemy.attack);
 
     // remove enemy's health by subtracting the amount we set in the damage variable
@@ -148,6 +150,9 @@ var fight = function(enemy) {
       window.alert(playerInfo.name + ' still has ' + playerInfo.health + ' health left.');
     }
   }
+  // switch turn order for the next round
+  isPlayerTurn = !isPlayerTurn;
+}
 };
 
 // go to shop between battles function
